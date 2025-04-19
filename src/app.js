582eabef6920a5a3fd6ae1b7e5d268291f9acb78
Awan Ardy <muhammadawan46@gmail.com>
2025-04-19 17:42:33 +0800

    tyddtdu6r

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const authRoutes = require("./routes/auth");
 const kosRoutes = require("./routes/kost.routes");
 
-app.use(require("cors")());
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 
 app.use("/api/auth", authRoutes);
