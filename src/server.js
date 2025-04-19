d6849d4c5bf4dc0a58b1df7eb015e86e6cdae2fb
Awan Ardy <muhammadawan46@gmail.com>
2025-04-19 17:59:07 +0800

    still cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const server = express();
 const authRoutes = require("./routes/auth");
 const kosRoutes = require("./routes/kost.routes");
